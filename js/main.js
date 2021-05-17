a4c3ba27076c753aee20237cb8b369dc24140431
igorek_chernov_1998@mail.ru <you@example.comigorek_chernov_1998@mail.ru>
2021-05-17 09:41:33 +0300

    changed files

--- js/main.js
+++ js/main.js
@@ -194,42 +194,6 @@
     }; // end ssSwiper
 
 
-   /* Lightbox
-    * ------------------------------------------------------ */
-    const ssLightbox = function() {
-
-        const folioLinks = document.querySelectorAll('.folio-item a');
-        const modals = [];
-
-        folioLinks.forEach(function(link) {
-            let modalbox = link.getAttribute('href');
-            let instance = basicLightbox.create(
-                document.querySelector(modalbox),
-                {
-                    onShow: function(instance) {
-                        //detect Escape key press
-                        document.addEventListener("keydown", function(evt) {
-                            evt = evt || window.event;
-                            if(evt.keyCode === 27){
-                            instance.close();
-                            }
-                        });
-                    }
-                }
-            )
-            modals.push(instance);
-        });
-
-        folioLinks.forEach(function(link, index) {
-            link.addEventListener("click", function(e) {
-                e.preventDefault();
-                modals[index].show();
-            });
-        });
-
-    };  // end ssLightbox
-
-
    /* Alert boxes
     * ------------------------------------------------------ */
     const ssAlertBoxes = function() {
@@ -307,7 +271,6 @@
         ssMobileMenu();
         ssScrollSpy();
         ssSwiper();
-        ssLightbox();
         ssAlertBoxes();
         ssSmoothScroll();
         ssBackToTop();
